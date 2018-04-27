4f935d52621361646c4e373b9bad2997ec4d8fc0
Katherine Guerrero <kfguerrero@uc.cl>
2018-04-27 14:03:10 -0300

    ProblemSet validation end_time bigger than start_time

--- server/models/problem-set.js
+++ server/models/problem-set.js
@@ -2,4 +2,18 @@
 
 module.exports = function(Problemset) {
 
+  const biggerDate = function(start_date, end_date){
+    return(end_date > start_date)
+  }
+
+  const validateBiggerDate = function(err){
+    if(!biggerDate(this.start_time, this.end_time)){
+      err()
+    }
+  }
+
+  Problemset.validate('end_time', validateBiggerDate, {
+    message: 'End time should be bigger than the start time',
+  });
+
 };
