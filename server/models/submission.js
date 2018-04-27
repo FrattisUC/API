821a5056ab84f6c8aff0ebeae41172634d341d4b
Katherine Guerrero <kfguerrero@uc.cl>
2018-04-27 18:20:41 -0300

    Submission status validation

--- server/models/submission.js
+++ server/models/submission.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Submission) {
+  
+  // Validación status
+  const statusList = {"0" : "pending", "1" : "done", "2" : "failure"}
+  
+  const validStatus = function(status){
+    return(!!statusList[status])
+  }
+
+  const validateStatus = function(err){
+    if(!validStatus(this.status)){
+      err()
+    }
+  }
+
+  Submission.validate('status', validateStatus, {
+    message: 'Status invalid',
+  });
+
+  // validación url_content
+
 
 };
