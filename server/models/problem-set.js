96f61e7add49796d56ad4d34cb65211c983df66b
Katherine Guerrero <kfguerrero@uc.cl>
2018-04-29 17:33:00 -0300

    style fix and comments

--- server/models/problem-set.js
+++ server/models/problem-set.js
@@ -3,14 +3,14 @@
 module.exports = function(Problemset) {
 
   const biggerDate = function(start_date, end_date){
-    return(end_date > start_date)
-  }
+    return(end_date > start_date);
+  };
 
   const validateBiggerDate = function(err){
     if(!biggerDate(this.start_time, this.end_time)){
-      err()
+      err();
     }
-  }
+  };
 
   Problemset.validate('end_time', validateBiggerDate, {
     message: 'End time should be bigger than the start time',
--- server/models/problem.js
+++ server/models/problem.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Decidimos que para crear un test se le va a crear AL problema
+  // Los tests no se van a crear por si solos y luego agregar
+
 module.exports = function(Problem) {
 
 };
