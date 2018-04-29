95911a49bfc17136697d978c19ef9c562ff0b042
Katherine Guerrero <kfguerrero@uc.cl>
2018-04-29 17:40:14 -0300

    Not validations to add to Problem

--- server/models/problem.js
+++ server/models/problem.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // Decidimos que para crear un test se le va a crear AL problema
-  // Los tests no se van a crear por si solos y luego agregar
+// Los tests no se van a crear por si solos y luego agregar
 
 module.exports = function(Problem) {
-
+  // Se puede poner cualquier nombre al problema - No tiene validación name
+  // El largo de la descripción del problema no está limitado - No hay
+  // validación para description
+  
 };
--- server/models/test.js
+++ server/models/test.js
@@ -1,6 +1,6 @@
 'use strict';
 // Decidimos que para crear un test se le va a crear AL problema
- // Los tests no se van a crear por si solos y luego agregar
+// Los tests no se van a crear por si solos y luego agregar
 module.exports = function(Test) {
 
 };
