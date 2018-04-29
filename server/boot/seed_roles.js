57e9dd0070a6bacb8044670345d545667f9c3586
Katherine Guerrero <kfguerrero@uc.cl>
2018-04-29 17:28:43 -0300

    seed does not repeat instances

--- server/boot/seed_roles.js
+++ server/boot/seed_roles.js
@@ -1,7 +1,9 @@
+'use strict';
 var async = require('async');
 var psqlDatasourceName = 'psql_db';
 
 module.exports = function(app) {
+  
   //data sources
   var psqlDs = app.dataSources[psqlDatasourceName];
   //create all models
@@ -11,19 +13,28 @@ module.exports = function(app) {
     if (err) throw err;
     console.log('> models created sucessfully');
   });
-  //create reviewers
+
+  //create roles
   function createRoles(cb) {
     psqlDs.automigrate('Role', function(err) {
       if (err) return cb(err);
       var Role = app.models.HumanRole;
-      Role.create([
-        {name: 'Superadmin'},
-        {name: 'Admin'},
-        {name: 'Teacher'},
-        {name: 'TeacherAssistant'},
-        {name: 'Student'},
-        {name: 'UnregisteredStudent'}
-      ], cb);
+      // Role.destroyAll();
+      const rolesArray = ['Superadmin', 'Admin','Teacher',
+                          'TeacherAssistant','Student',
+                          'UnregisteredStudent'];
+      rolesArray.forEach(function(i){
+        Role.findOrCreate({name: i});
+      });
+      
+      // Role.create([
+      //   {name: 'Superadmin'},
+      //   {name: 'Admin'},
+      //   {name: 'Teacher'},
+      //   {name: 'TeacherAssistant'},
+      //   {name: 'Student'},
+      //   {name: 'UnregisteredStudent'}
+      // ], cb);
     });
   }
 
