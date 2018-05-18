25e907a839bf9c2a4525b0b19b901b213e47973f
Katherine Guerrero <kfguerrero@uc.cl>
2018-05-18 13:21:12 -0400

    test for valid time differences in problemSet

--- test/unit/problem-set.test.js
+++ test/unit/problem-set.test.js
@@ -9,11 +9,21 @@ describe("Validation", function(){
 
   it('should reject an endTime bigger than startTime', function(){
 
-    return Problemset.create({ name: "test-problemset", startTime: "18/05/2017", endTime: "18/02/2017"})
+    return Problemset.create({ name: "test-problemset", startTime: "18/05/2017", endTime: "18/02/2017" })
       .then(res => Promise.reject('ProblemSet should not be created'))
       .catch(err => {
         expect(err.message).to.contain("End time should be bigger than the start time");
         expect(err.statusCode).to.be.equal(422);
+      });
+
+  });
+
+  // Test validación correcta de times
+  it('should accept valid endTime', function(){
+    return Problemset.create({ name: "test-problemset", startTime: "18/05/2017", endTime: "18/07/2017" })
+      .then(res => {
+        expect(res.startTime).to.equal("18/05/2017");
+        expect(res.endTime).to.equal("18/07/2017");
       });
 
   });
