46bf40212fcd20c0c1bd15ea006a4d1f020aa863
Katherine Guerrero <kfguerrero@uc.cl>
2018-05-18 12:56:43 -0400

    human tests

--- test/unit/human.test.js
+++ test/unit/human.test.js
@@ -5,7 +5,8 @@ const { app, expect } = require('../common');
 // Get a reference to the Human model
 const Human = app.models.Human;
 
-describe("It should", function(){
+describe("Validation", function(){
+  // Test validación duplicados
   it('should reject a duplicate email', function(){
     return Promise.resolve()
       .then(() => Human.create({ email: 'repeated-email@email.com'}))
@@ -17,4 +18,25 @@ describe("It should", function(){
       });
 
   });
+
+  // Test validación incorrecto email
+  it('should reject an invalid email', function(){
+    return Human.create({ email: "email@invalido@gmail.com"})
+      .then(res => Promise.reject('Human should not be created'))
+      .catch(err => {
+        expect(err.message).to.contain("Email invalid");
+        expect(err.statusCode).to.be.equal(422);
+      });
+
+  });
+
+  // Test validación correcto email
+  it('should accept valid email', function(){
+    return Human.create({ email: "valido@gmail.com"})
+      .then(res => {
+        expect(res.email).to.equal("valido@gmail.com");
+      });
+
+  });
+
 });
